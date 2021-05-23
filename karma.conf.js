fab04d01d6da3dcbd21181844d6e0f09f2b4e6bc
haidner1 <haidner@live.de>
2021-05-23 12:25:15 +0200

    karma config changed

--- karma.conf.js
+++ karma.conf.js
@@ -37,8 +37,12 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
